c3195903c6b9c78cfe5ef216f4c62f0a306bd0b8
Juan Francisco <contrerasjf.0@hotmail.com>
2019-09-23 15:18:12 -0500

    Updates

---
+++ precache-manifest.99a173d20ecff25be9ecb5249fb71c9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c8fe7bb0aa1827a605ba393d3bd647",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f2f5d243237a5eadb0db",
+    "url": "/portfolio/static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "e007c54e5c4853c60efd",
+    "url": "/portfolio/static/css/main.82cb674a.chunk.css"
+  },
+  {
+    "revision": "f2f5d243237a5eadb0db",
+    "url": "/portfolio/static/js/2.69f6e17e.chunk.js"
+  },
+  {
+    "revision": "e007c54e5c4853c60efd",
+    "url": "/portfolio/static/js/main.a457eb1e.chunk.js"
+  },
+  {
+    "revision": "e62b1861ebc643f976ce",
+    "url": "/portfolio/static/js/runtime~main.0b42a60c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
